b0a9fcaee847b0c32d99fb2ab7999c532c5940c7
Sean Haile <seanhaile.codes@gmail.com>
2020-03-11 23:34:24 -0600

    add iffy to page

---
+++ Exercise Files/Ch02/02_01/start/app.js
@@ -0,0 +1,7 @@
+(function() {
+  "use strict";
+  var ProductCustmoizer = React.createElement ('div', { className: 'customizer' }, "Product info goes here")
+  // Start here
+
+  ReactDOM.render(ProductCustmoizer, document.getElementById('react-root'))
+})();
